5bbe135ea04897d0b2dd4e0314f8e6207edffba3
Grégory OBANOS <gregory.obanos@gmail.com>
2021-02-06 14:30:04 +0100

    Excercice 1.1, part 3

--- examples/exercice1_1.rs
+++ examples/exercice1_1.rs
@@ -8,29 +8,56 @@ fn reverse(content: &str) {
     println!("Reverse the file content by storing it in a LIFO (VecDeque): ");
     let lines: VecDeque<&str> = content.lines().collect();
     for (i, line) in lines.into_iter().rev().enumerate() {
-        println!("{}: {}", i, line);
+        println!("{}: '{}'", i + 1, line);
     }
 }
 
-fn reverse_bulk_50(content: &str) {
+fn reverse_bulk(content: &str) {
     const BULK_SIZE: usize = 50;
     println!(
         "Reverse the file content {} lines at a time by storing it in a LIFO (VecDeque): ",
         BULK_SIZE
     );
-    let mut buffer = VecDeque::with_capacity(50);
+    let mut buffer = VecDeque::with_capacity(BULK_SIZE);
+    let original_capacity = buffer.capacity();
     let lines = &mut content.lines();
 
     for bulk_index in 0.. {
         buffer.clear();
         buffer.extend(lines.take(BULK_SIZE));
         if buffer.is_empty() {
-            return;
+            break;
         }
         for (i, line) in buffer.iter().rev().enumerate() {
-            println!("{}: {}", i + BULK_SIZE * bulk_index, line);
+            println!("{}: '{}'", i + BULK_SIZE * bulk_index + 1, line);
         }
     }
+    assert_eq!(buffer.capacity(), original_capacity);
+}
+
+fn fill_blanks(content: &str) {
+    const BUFFER_SIZE: usize = 42;
+    println!(
+        "Replace blanks lines (after line {size}) with the one {size} lines before using a Deque (VecDeque)",
+        size = BUFFER_SIZE
+    );
+    let mut buffer = VecDeque::with_capacity(BUFFER_SIZE);
+    let original_capacity = buffer.capacity();
+    let lines = &mut content.lines();
+
+    // Fill the buffer
+    for (i, line) in lines.take(BUFFER_SIZE).enumerate() {
+        buffer.push_back(line);
+        println!("{}: '{}'", i + 1, line);
+    }
+    // Run with blank detection
+    for (i, line) in lines.enumerate() {
+        let default = buffer.pop_front().expect("buffer is empty");
+        let line = if line.is_empty() { default } else { line };
+        buffer.push_back(line);
+        println!("{}: '{}'", i + BUFFER_SIZE + 1, line);
+    }
+    assert_eq!(buffer.capacity(), original_capacity);
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
@@ -44,7 +71,8 @@ fn run() -> Result<(), Box<dyn Error>> {
     let file_content = read_to_string(&file)?;
 
     reverse(&file_content);
-    reverse_bulk_50(&file_content);
+    reverse_bulk(&file_content);
+    fill_blanks(&file_content);
 
     Ok(())
 }
